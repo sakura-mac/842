38f0fe201ad6383b99e97ec8df911cfd36d1b69b
khalilchen <1760237725@qq.com>
2022-08-21 17:20:09 +0800

    init

---
+++ c/main.c
@@ -0,0 +1,13 @@
+/*
+* coding = utf-8
+* @Author : Khalil Chen
+* @Created Time : 2022-08-21 16:56:10
+* @Description : 
+*/
+#include "stdlib.h" 
+#include "stdio.h" 
+int main(int argc, char *const* argv)
+{
+    //begin main code
+    return 0;
+}
