63afacb495e786b5b9d6bf494a5f9c8fba64f0db
khalilchen <1760237725@qq.com>
2022-09-24 11:39:24 +0800

    pointer test

--- c/main.c
+++ c/main.c
@@ -6,9 +6,20 @@
 */
 #include "stdlib.h" 
 #include "stdio.h" 
+
+struct s 
+{
+    int n;
+    struct s *next;
+};
 int main(int argc, char *const* argv)
 {
     //begin main code
+    struct s a[3] = {10, &a[1], 5, &a[2], 9, '\0'}, *p;
+    p = &a[0];
     printf("hello world! \n");
+    printf("%d %d", p++->n, ++p->n);
+
     return 0;
+    
 }
