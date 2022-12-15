8b75c93380bd3521a5b899f679c80d8fc48a3ec2
khalilchen <1760237725@qq.com>
2022-12-15 17:12:28 +0800

    add delete string

--- c/c_al.c
+++ c/c_al.c
@@ -44,37 +44,79 @@ void caculate(){
 
 }
 
-/*
-* @Description: put file 
-* @Arguments:
-* @Return: 
-*/
-void PutFile(){
-	FILE *fp;
-	char buf;
-	int count = 0;
-	fp = fopen("1.txt", "r");
-	if(fp == NULL){
-		printf("no file\n");
-		exit(0);
-	}
+int FirstIndex(char *p1, char *p2, int len2){
+	int ret = -1;
+	int i = 0;
+	//find every first letter *(p1+i) and  *(p2)
+	  //for every letter, cmp *(p1+i+k) and *(p2+k)
+	    //if yes, ret = i;break;
+		//if no, k = 1; continue loop
+	for( ; p1[i] != '\0'; i++){
+		if(p1[i] == p2[0] ){
+			int k = 1;
+			for( ; k < len2; k++){
+				if(p1[i+k] != p2[k]){
+					break;
+				}
+			}
 
-	while(fscanf(fp, "%c", &buf) > 0){
-		count++;
-		printf("%c", buf);
-		if(buf != '\n'){
-			count = 0;
-			continue;
+			if(k != len2){
+				k = 1;
+			}else{
+				ret = i;
+				break;
+			}
 		}
-		if(count % 30 == 0){
-			printf("\n");
-		}
-		fclose(fp);
 	}
+	return ret;
+}
+void Copy(char **ret, char *p1, int i){
+	if(i == -1){
+		//fill all
+		while(*p1 != '\0'){
+			**(ret) = *p1++;
+			*ret += 1;
+		}
+	}else{
+		//fill [p1, p1+i)
+		for(int k = 0; k < i;(*ret)++, k++){
+			**(ret) = *(p1+k);
+		}	
+
+	}
+}
+char *delstring(char *str1, char *str2){
+	char *ret = (char *)malloc(sizeof(char)*100);
+	char *p0 = ret;
+	char *p1 = str1;
+	char *p2 = str2;
+	//len2
+	int len2 = 0;
+	while(*p2 != '\0'){
+		p2++;
+		len2++;
+	}
+	p2 = str2;
+	while(1){
+		int i = FirstIndex(p1, p2, len2);//relative i
+	//	printf("this i: %d\n", i);
+		if(i != -1){
+			Copy(&p0, p1, i);//
+			p1 = p1+i+len2;
+		}else{
+			Copy(&p0, p1, i);
+			break;
+		}
+	}
+	return ret;
 }
 int main(int argc, char *const* argv)
 {
     //begin main code
+	char *s1 = "helloFworldFheloFworldlala";
+	char *s2 = "lala";
+	char *ret = delstring(s1, s2);
+	printf("%s", ret);
     return 0;
     
 }
